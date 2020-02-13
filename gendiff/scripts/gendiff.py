5a5004e100833f0a96e850c5a99f67ea5bc1bd34
belousovromnik <belousovromnik@yandex.ru>
2020-02-13 12:44:20 +0300

    изменения после шага 1

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,16 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Generate diff')
+parser.add_argument('first_file', help='first file to compare')
+parser.add_argument('second_file', help='second file to compare')
+parser.add_argument('-f', '--format', help='set format of output')
+
+args = parser.parse_args()
+
 
 def main():
-    print('Gendiff!!!!!!!!!!!!!!!!!!!')
+    print('first_file - {}'.format(args.first_file))
+    print('second_file - {}'.format(args.second_file))
 
 
 if __name__ == '__main__':
